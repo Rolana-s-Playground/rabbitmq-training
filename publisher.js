80869f661b56b4978220f7ac4b9b71e0a6ad8e2f
Maria Mikhalska <rolana93@gmail.com>
2021-11-23 13:34:50 +0200

    create publisher

---
+++ publisher.js
@@ -0,0 +1,17 @@
+const amqp = require('amqplib');
+
+const msg = { number: 2 };
+
+async function connect() {
+    try {
+        const connection = await amqp.connect('amqp://localhost:56721');
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue('jobs');
+        channel.sendToQueue('jobs', Buffer.from(JSON.stringify(msg)));
+        console.log(`Job sent successfully ${msg.number}`);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+connect();
